1c4ce12c6a80428de83f150a0fd9977fffed0b30
fds-mail <m.firdaus3130@gmail.com>
2023-06-19 13:12:57 +0800

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      vt323: ["VT323", "monospace"],
+      bungee: ["Bungee", "display"],
+      abel: ["Abel", "sans-serif"],
+      bayon: ["Bayon", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
